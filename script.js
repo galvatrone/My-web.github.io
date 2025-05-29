3cd6dd9e2486303ec7ef60c8decf35bf3bb54092
galvatrone <103832950+galvatrone@users.noreply.github.com>
2025-05-29 21:07:06 +0300

    Обновлён HTML и CSS для улучшения структуры и дизайна портфолио. Добавлены новые секции, изменены стили, улучшена адаптивность и анимация. Реализовано движение дымовых эффектов при прокрутке.

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // Удаляем intro после анимации
 setTimeout(() => {
-  document.querySelector('.intro-screen').style.display = 'none';
+  const intro = document.querySelector('.intro-screen');
+  if (intro) intro.style.display = 'none';
 }, 5000); // 5 секунд (анимация + пауза)
+
+ window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    document.documentElement.style.setProperty('--smoke-move-left', `${scrollY * 0.3}px`);
+    document.documentElement.style.setProperty('--smoke-move-right', `${-scrollY * 0.3}px`);
+  });
+
+   const smokeLeft = document.querySelector('.smoke-left');
+  const smokeRight = document.querySelector('.smoke-right');
+
+  window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+
+    smokeLeft.style.transform = `translateY(${scroll * 0.3}px) translateX(-20px)`;
+    smokeRight.style.transform = `translateY(${-scroll * 0.3}px) translateX(20px)`;
+  });
